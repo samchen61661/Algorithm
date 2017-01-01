ea4baf8d076e0f14b960e616c53367efa79023cb
samchen <samchen61661@gmail.com>
2017-01-01 20:10:11 +0800

    fix bug

--- Trie/WordSearch.java
+++ Trie/WordSearch.java
@@ -4,6 +4,10 @@ public class Solution {
      * @param word: A string
      * @return: A boolean
      */
+     
+    private final int[] deltaX = {-1, 0, 1, 0};
+    private final int[] deltaY = {0, 1, 0, -1};
+    
     public boolean exist(char[][] board, String word) {
         if (word == null || word.length() == 0) {
             return false;
@@ -20,50 +24,47 @@ public class Solution {
             return false;
         }
         
+        
+        // DFS
+        boolean[][] isVisited = new boolean[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (dfs(board, word, i, j, new StringBuilder())) {
+                if (dfs(board, word, i, j, new StringBuilder(), isVisited)) {
                     return true;
                 }
             }
         }
         
         return false;
+      
     }
     
-    private boolean dfs (char[][] board, String word, int i, int j, StringBuilder builder) {
+    private boolean dfs (char[][] board, String word, int i, int j, StringBuilder builder, boolean[][] isVisited) {
         if (word.length() == builder.length()) {
             return true;
         }
         
         int row = board.length;
         int col = board[0].length;
-        
-        if (i >= 0 && j >= 0 && i < row && j < col && board[i][j] != '0') {
+        if (i >= 0 && j >= 0 && i < row && j < col && !isVisited[i][j]) {
             if (word.charAt(builder.length()) == board[i][j]) {
                 builder.append(board[i][j]);
-                char c = board[i][j];
-                board[i][j] = '0';
+                isVisited[i][j] = true;
                 
-                if (dfs(board, word, i + 1, j, builder)) {
+                for (int k = 0; k < 4; k++) {
+                    int new_x = i + deltaX[k];
+                    int new_y = j + deltaY[k];
+                    if (dfs(board, word, new_x, new_y, builder, isVisited)) {
                     return true;
                 }
-                if (dfs(board, word, i - 1, j, builder)) {
-                    return true;
                 }
-                if (dfs(board, word, i, j - 1, builder)) {
-                    return true;
-                }
-                if (dfs(board, word, i, j + 1, builder)) {
-                    return true;
-                }  
                 
-                board[i][j] = c;
+                isVisited[i][j] = false;
                 builder.setLength(builder.length() - 1);
             }
+            
         }
         
         return false;
     }
 }
-
