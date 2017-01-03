d43ffdeeea31501312ccc2009ecb556ec6be304b
samchen <samchen61661@gmail.com>
2017-01-03 17:05:03 +0800

    Partition Array by Odd and Even
    Partition an integer array into odd number first and even number second.

---
+++ Two Pointers/PartitionArrayByOddAndEven.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: nothing
+     */
+    public void partitionArray(int[] nums) {
+        if (nums == null) {
+            return;
+        }
+        
+        int len = nums.length;
+        
+        int i = 0; 
+        int j = len - 1;
+        
+        while (i < j) {
+            while (i < j && (nums[i] % 2 == 1)) {
+                i++;
+            }
+            while (i < j && (nums[j] % 2 == 0)) {
+                j--;
+            }
+            swap(nums, i, j);
+            i++; 
+            j--;
+        }
+    }
+    
+    private void swap (int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
