b6affc9e8ba6f4a41fce0d94865093e11fb95363
samchen <samchen61661@gmail.com>
2017-01-07 11:30:21 +0800

    Sort Letters by Case
    Given a string which contains only letters. Sort it by lower case first and upper case second.

---
+++ Two Pointers/SortLettersByCase.java
@@ -0,0 +1,35 @@
+public class Solution {
+    /** 
+     *@param chars: The letter array you should sort by Case
+     *@return: void
+     */
+    public void sortLetters(char[] chars) {
+        if (chars == null || chars.length < 2) {
+            return;
+        }
+        
+        int len = chars.length;
+        int left = 0;
+        int right = len - 1;
+        
+        while (left < right) {
+            while (left < right && chars[right] < 'a') {
+                right--;
+            }
+            
+            while (left < right && chars[left] >= 'a') {
+                left++;
+            }
+            
+            swap(chars, left, right);
+            left++;
+            right--;
+        }
+    }
+    
+    private void swap (char[] chars, int i, int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+}
