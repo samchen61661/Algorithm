624ee0cc4243f81db07976e59ee269b4d51ca48a
samchen <samchen61661@gmail.com>
2017-01-07 13:46:12 +0800

    fix bug

--- Two Pointers/PartitionArrayByOddAndEven.java
+++ Two Pointers/PartitionArrayByOddAndEven.java
@@ -13,16 +13,18 @@ public class Solution {
         int i = 0; 
         int j = len - 1;
         
-        while (i < j) {
-            while (i < j && (nums[i] % 2 == 1)) {
+        while (i <= j) {
+            while (i <= j && (nums[i] % 2 == 1)) {
                 i++;
             }
-            while (i < j && (nums[j] % 2 == 0)) {
+            while (i <= j && (nums[j] % 2 == 0)) {
                 j--;
             }
-            swap(nums, i, j);
-            i++; 
-            j--;
+            if (i <= j) {
+                swap(nums, i, j);
+                i++; 
+                j--;
+            }
         }
     }
     
