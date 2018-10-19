201b35328b86e6f7990281b3b325b0f94c19834d
samchen <samchen61661@gmail.com>
2018-10-19 11:09:41 -0700

    Update 3Sum Closest

--- Two Pointers/3SumClosest.java
+++ Two Pointers/3SumClosest.java
@@ -1,39 +1,31 @@
-public class Solution {
+class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int len = nums.length;
-        if (len < 3) {
-            return -1;
+        if (nums == null || nums.length < 3) {
+            return Integer.MAX_VALUE;
         }
-        
+        int res = nums[0] + nums[1] + nums[2];
         Arrays.sort(nums);
-        int res = nums[0] + nums[1] + nums[2];   // locality
-        
-        for (int i = 0; i < len - 2; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                int j = i + 1;
-                int k = len - 1;
-                
-                while (j < k) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    res = (Math.abs(target - sum) < Math.abs(target - res)) ? sum : res;
-                    
-                    if (sum == target) {
-                        return target;
-                    } else if (sum < target) {
-                        while (j < k && nums[j] == nums[j + 1]) {
-                            j++;
-                        }
-                        j++;
-                    } else {
-                        while (j < k && nums[k] == nums[k - 1]) {
-                            k--;
-                        }
-                        k--;
-                    }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (target == sum) {
+                    return sum;
+                }
+                if (Math.abs(target - sum) < Math.abs(target - res)) {
+                    res = sum;
+                }
+                if (target > sum) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        
         return res;
     }
 }
